9c1334b1928d0bd125a541ea3a0c9baa6ea146a1
vijaykumarrpai <vijaykumarrpai@gmail.com>
2020-07-15 17:18:30 +0530

    Updates

---
+++ precache-manifest.4b94f9fc499309acfd50582ac9023029.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb780b9d75ccb5cb4ba9a2f780172e03",
+    "url": "/simple-weather-app/index.html"
+  },
+  {
+    "revision": "e93969db8b5bc51daf25",
+    "url": "/simple-weather-app/static/css/main.e18b2057.chunk.css"
+  },
+  {
+    "revision": "1b4e04382d758fdbfef6",
+    "url": "/simple-weather-app/static/js/2.d6aee609.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-weather-app/static/js/2.d6aee609.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e93969db8b5bc51daf25",
+    "url": "/simple-weather-app/static/js/main.4a691fd4.chunk.js"
+  },
+  {
+    "revision": "1751b747bde2556bb8b4",
+    "url": "/simple-weather-app/static/js/runtime-main.ed1cee73.js"
+  },
+  {
+    "revision": "db9ab18c84fa611c5202fe3fc8939136",
+    "url": "/simple-weather-app/static/media/Clear.db9ab18c.svg"
+  },
+  {
+    "revision": "a82005aa954c61cba1a50336159d70d8",
+    "url": "/simple-weather-app/static/media/Cloud-wind.a82005aa.svg"
+  },
+  {
+    "revision": "7ee70f9fbfac3ec8b5360a4b53007c00",
+    "url": "/simple-weather-app/static/media/Clouds.7ee70f9f.svg"
+  },
+  {
+    "revision": "425a2644e2584f241fddf022076368d8",
+    "url": "/simple-weather-app/static/media/Drizzle.425a2644.svg"
+  },
+  {
+    "revision": "9248a8c13cb45109b116df709501c0b3",
+    "url": "/simple-weather-app/static/media/Fog.9248a8c1.svg"
+  },
+  {
+    "revision": "82d93268572fedb433dfba27d9e52d21",
+    "url": "/simple-weather-app/static/media/Haze.82d93268.svg"
+  },
+  {
+    "revision": "ffbda6d96dd5f27897269bc514369328",
+    "url": "/simple-weather-app/static/media/Mist.ffbda6d9.svg"
+  },
+  {
+    "revision": "e3158b56e90d7c4ae2ac85e0ef7ed4bc",
+    "url": "/simple-weather-app/static/media/Moon.e3158b56.svg"
+  },
+  {
+    "revision": "06f78ffe921aff71ae391ae110b95c08",
+    "url": "/simple-weather-app/static/media/Night-rainy.06f78ffe.svg"
+  },
+  {
+    "revision": "7d9c9233f8d2a0402a90753501bfd21e",
+    "url": "/simple-weather-app/static/media/Night.7d9c9233.svg"
+  },
+  {
+    "revision": "29a0aca0c7b11b9e2e20e203bca66089",
+    "url": "/simple-weather-app/static/media/Preview.29a0aca0.svg"
+  },
+  {
+    "revision": "708290b4749a1d1d0e8f049c0e761778",
+    "url": "/simple-weather-app/static/media/Rain.708290b4.svg"
+  },
+  {
+    "revision": "c623741c0b9e188e5b6a93a5e1c0ad9e",
+    "url": "/simple-weather-app/static/media/Rainy-thunder.c623741c.svg"
+  },
+  {
+    "revision": "b5e8855f869aeaa037290f87cf347fd8",
+    "url": "/simple-weather-app/static/media/Rainy.b5e8855f.svg"
+  },
+  {
+    "revision": "104ea80ade6e3fd46928a77892b6d86c",
+    "url": "/simple-weather-app/static/media/Snow.104ea80a.svg"
+  },
+  {
+    "revision": "f963a166b3279d455437165cb78cce6a",
+    "url": "/simple-weather-app/static/media/Sun.f963a166.svg"
+  },
+  {
+    "revision": "a98369fb7ac4ac2d522c92642bb69bc8",
+    "url": "/simple-weather-app/static/media/Sunshine.a98369fb.svg"
+  },
+  {
+    "revision": "cd8d7fceea994a098c78b381e4e9916a",
+    "url": "/simple-weather-app/static/media/Tonado.cd8d7fce.svg"
+  }
+]);
